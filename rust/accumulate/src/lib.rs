10a0b6fa947b9cb4388d9f14718b1f7ab4b66daa
Tomislav Ivek <tomislav.ivek@gmail.com>
2018-10-13 13:52:59 +0200

    make accumulate lazy

--- rust/accumulate/src/lib.rs
+++ rust/accumulate/src/lib.rs
@@ -1,11 +1,38 @@
-pub fn map<V, F, I, O>(input: V, mut _function: F) -> Vec<O>
+pub struct Map<I, F> {
+    iter: I,
+    f: F,
+}
+
+impl<I, F, B> Iterator for Map<I, F>
 where
-    V: IntoIterator<Item = I>,
-    F: FnMut(I) -> O,
+    I: Iterator,
+    F: FnMut(I::Item) -> B,
 {
-    let mut out = Vec::new();
-    for i in input {
-        out.push(_function(i));
+    type Item = B;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match self.iter.next() {
+            None => None,
+            Some(a) => Some((self.f)(a)),
+        }
     }
-    out
+}
+
+pub fn map_lazy<C, F, A, B>(input: C, _function: F) -> Map<C::IntoIter, F>
+where
+    C: IntoIterator<Item = A>,
+    F: FnMut(A) -> B,
+{
+    Map {
+        iter: input.into_iter(),
+        f: _function,
+    }
+}
+
+pub fn map<C, F, A, B>(input: C, _function: F) -> Vec<B>
+where
+    C: IntoIterator<Item = A>,
+    F: FnMut(A) -> B,
+{
+    map_lazy(input, _function).collect()
 }
